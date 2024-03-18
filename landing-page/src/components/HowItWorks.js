9eec2ae916dbaf151bbcac761b8bb20f4d31b7fa
Nawaritta <Nor.benhaddou@gmail.com>
2024-03-19 00:31:08 +0200

    Revert "Merge branch 'main' into Nora"
    This reverts commit e43db303dd21b61a8171336746e4ac9747568a8d, reversing
    changes made to 80a965586353fe44541fa53e2639edf7180a249b.

--- landing-page/src/components/HowItWorks.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HowItWorks = () => {
-    return (
-        <div>HowItWorks</div>
-    )
-}
-
-export default HowItWorks
--- landing-page/src/pages/index.js
+++ landing-page/src/pages/index.js
@@ -1,22 +1,13 @@
 import { Inter } from "next/font/google";
-import NavBar from "@/components/NavBar";
-import Hero from "@/components/Hero";
-import HowItWorks from "@/components/HowItWorks";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <NavBar />
-      <Hero />
-      <HowItWorks />
-      <Contact />
-      <Footer />
+
     </main>
   );
 }
