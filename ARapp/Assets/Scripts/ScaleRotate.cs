c1da2bb74bd1088814f65a998afc92e03dd1cfe1
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-03-24 18:04:49 +0000

    reset sliders after performing actions on the oject
    Co-Authored-By: Nawaritta <125602343+Nawaritta@users.noreply.github.com>

--- ARapp/Assets/Scripts/ScaleRotate.cs
+++ ARapp/Assets/Scripts/ScaleRotate.cs
@@ -1,69 +1,60 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
 
 public class ScaleRotate : MonoBehaviour
 {
     private Slider scale;
     private Slider rotate;
-   /*
-    public float scaleMin;
-    public float scaleMax;
-    public float rotateMin;
-    public float rotateMax;
-   */
     private Slider xSlider, ySlider, zSlider;
-    
+    private GameObject currentItem;
+
+
 
     public bool scriptEnabled = true;
 
     void Start()
     {
+       
         scale = GameObject.Find("ScaleSlider").GetComponent<Slider>();
-       // scale.maxValue = scaleMax;
-        //scale.minValue = scaleMin;
         scale.onValueChanged.AddListener(ScaleUpdate);
 
         rotate = GameObject.Find("RotateSlider").GetComponent<Slider>();
-        //rotate.maxValue = rotateMax;
-        //rotate.minValue = rotateMin;
         rotate.onValueChanged.AddListener(RotateUpdate);
 
         xSlider = GameObject.Find("xSlider").GetComponent<Slider>();
-        xSlider.maxValue += transform.position.x;
-        xSlider.minValue += transform.position.x;
         xSlider.onValueChanged.AddListener(XUpdate);
 
         ySlider = GameObject.Find("ySlider").GetComponent<Slider>();
-        ySlider.maxValue += transform.position.y;
-        ySlider.minValue += transform.position.y;
         ySlider.onValueChanged.AddListener(YUpdate);
 
         zSlider = GameObject.Find("zSlider").GetComponent<Slider>();
-        zSlider.maxValue += transform.position.z;
-        zSlider.minValue += transform.position.z;
         zSlider.onValueChanged.AddListener(ZUpdate);
 
         Button confirmButton = GameObject.Find("ConfirmBtn").GetComponent<Button>();
         confirmButton.onClick.AddListener(ToggleScriptEnabled);
     }
 
+    
     void ToggleScriptEnabled()
     {
         scriptEnabled = !scriptEnabled;
+        zSlider.value = (zSlider.minValue + zSlider.maxValue) / 2f;
+        ySlider.value = (ySlider.minValue + ySlider.maxValue) / 2f;
+        xSlider.value = (xSlider.minValue + xSlider.maxValue) / 2f;
 
-        zSlider.maxValue += transform.position.z;
-        zSlider.minValue += transform.position.z;
-        ySlider.maxValue += transform.position.y;
-        ySlider.minValue += transform.position.y;
-        xSlider.maxValue += transform.position.x;
-        xSlider.minValue += transform.position.x;
+        scale.value = 1; 
+        rotate.value = 1;
     }
-
+    
     public void ScaleUpdate(float value)
     {
-        if (!scriptEnabled)
-            return;
-        transform.localScale = new Vector3(value, value, value);
+        //if (!scriptEnabled)
+        // return;
+
+        currentItem = FindObjectOfType<FurniturePlacement>().currentItem;
+        if (this.gameObject == currentItem)
+            transform.localScale = new Vector3(value, value, value);            
 
     }
 
@@ -71,28 +62,36 @@ public class ScaleRotate : MonoBehaviour
     {
         if (!scriptEnabled)
             return;
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, value, transform.localEulerAngles.z);
+        currentItem = FindObjectOfType<FurniturePlacement>().currentItem;
+        if (this.gameObject == currentItem)
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, value, transform.localEulerAngles.z);
     }
 
     public void XUpdate(float value)
     {
         if (!scriptEnabled)
             return;
-        transform.position = new Vector3(value, transform.position.y, transform.localPosition.z);
+        currentItem = FindObjectOfType<FurniturePlacement>().currentItem;
+        if (this.gameObject == currentItem)
+            transform.position = new Vector3(value, transform.position.y, transform.localPosition.z);
     }
 
     public void YUpdate(float value)
     {
         if (!scriptEnabled)
             return;
-     
-        transform.position = new Vector3(transform.position.x, value, transform.localPosition.z);
+
+        currentItem = FindObjectOfType<FurniturePlacement>().currentItem;
+        if (this.gameObject == currentItem)
+            transform.position = new Vector3(transform.position.x, value, transform.localPosition.z);
     }
 
     public void ZUpdate(float value)
     {
         if (!scriptEnabled)
             return;
-        transform.position = new Vector3(transform.position.x, transform.position.y, value);
+        currentItem = FindObjectOfType<FurniturePlacement>().currentItem;
+        if (this.gameObject == currentItem)
+            transform.position = new Vector3(transform.position.x, transform.position.y, value);
     }
 }
