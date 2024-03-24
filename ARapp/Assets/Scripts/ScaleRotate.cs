df550fe31010775e0ed7fc35a8b7ac0199a8afbf
Nawaritta <Nor.benhaddou@gmail.com>
2024-03-24 01:35:39 +0000

    adding change position & fixing minor bugs
    fixing the panel
    
    Co-Authored-By: EL FADILI Abdessamad <105720445+elfadili-ae@users.noreply.github.com>

--- ARapp/Assets/Scripts/ScaleRotate.cs
+++ ARapp/Assets/Scripts/ScaleRotate.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,33 +5,58 @@ public class ScaleRotate : MonoBehaviour
 {
     private Slider scale;
     private Slider rotate;
+   /*
     public float scaleMin;
     public float scaleMax;
-    public float rotatMin;
-    public float rotatMax;
+    public float rotateMin;
+    public float rotateMax;
+   */
+    private Slider xSlider, ySlider, zSlider;
+    
+
     public bool scriptEnabled = true;
 
     void Start()
     {
         scale = GameObject.Find("ScaleSlider").GetComponent<Slider>();
-        scale.maxValue = scaleMax;
-        scale.minValue = scaleMin;
+       // scale.maxValue = scaleMax;
+        //scale.minValue = scaleMin;
         scale.onValueChanged.AddListener(ScaleUpdate);
 
         rotate = GameObject.Find("RotateSlider").GetComponent<Slider>();
-        rotate.maxValue = rotatMax;
-        rotate.minValue = rotatMin;
+        //rotate.maxValue = rotateMax;
+        //rotate.minValue = rotateMin;
         rotate.onValueChanged.AddListener(RotateUpdate);
+
+        xSlider = GameObject.Find("xSlider").GetComponent<Slider>();
+        xSlider.maxValue += transform.position.x;
+        xSlider.minValue += transform.position.x;
+        xSlider.onValueChanged.AddListener(XUpdate);
+
+        ySlider = GameObject.Find("ySlider").GetComponent<Slider>();
+        ySlider.maxValue += transform.position.y;
+        ySlider.minValue += transform.position.y;
+        ySlider.onValueChanged.AddListener(YUpdate);
+
+        zSlider = GameObject.Find("zSlider").GetComponent<Slider>();
+        zSlider.maxValue += transform.position.z;
+        zSlider.minValue += transform.position.z;
+        zSlider.onValueChanged.AddListener(ZUpdate);
 
         Button confirmButton = GameObject.Find("ConfirmBtn").GetComponent<Button>();
         confirmButton.onClick.AddListener(ToggleScriptEnabled);
-        // touch object --> scriptEnabled = true
-
     }
 
-     void ToggleScriptEnabled()
+    void ToggleScriptEnabled()
     {
-        scriptEnabled = false;
+        scriptEnabled = !scriptEnabled;
+
+        zSlider.maxValue += transform.position.z;
+        zSlider.minValue += transform.position.z;
+        ySlider.maxValue += transform.position.y;
+        ySlider.minValue += transform.position.y;
+        xSlider.maxValue += transform.position.x;
+        xSlider.minValue += transform.position.x;
     }
 
     public void ScaleUpdate(float value)
@@ -41,12 +64,35 @@ public class ScaleRotate : MonoBehaviour
         if (!scriptEnabled)
             return;
         transform.localScale = new Vector3(value, value, value);
+
     }
 
     public void RotateUpdate(float value)
     {
         if (!scriptEnabled)
             return;
-        transform.localEulerAngles = new Vector3(transform.rotation.x, value, transform.rotation.z);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, value, transform.localEulerAngles.z);
+    }
+
+    public void XUpdate(float value)
+    {
+        if (!scriptEnabled)
+            return;
+        transform.position = new Vector3(value, transform.position.y, transform.localPosition.z);
+    }
+
+    public void YUpdate(float value)
+    {
+        if (!scriptEnabled)
+            return;
+     
+        transform.position = new Vector3(transform.position.x, value, transform.localPosition.z);
+    }
+
+    public void ZUpdate(float value)
+    {
+        if (!scriptEnabled)
+            return;
+        transform.position = new Vector3(transform.position.x, transform.position.y, value);
     }
 }
